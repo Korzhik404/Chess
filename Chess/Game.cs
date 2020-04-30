97eead5e0ac781fc6d30147a57f4042cc717b5b6
Korzhik404 <63220161+Korzhik404@users.noreply.github.com>
2020-04-30 12:07:38 +0700

    Добавьте файлы проекта.

---
+++ Chess/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class Game
+    {
+        public bool ChessOver = false;
+        public int x = 0, y = 0, i = 0, z = 0;
+        public string[,] MyBoard = new string[,]
+        {
+            { "\u265c ","\u265e ","\u265d ", "\u265b ", "\u265a ", "\u265d ", "\u265e ", "\u265c " , " 8" },
+            { "\u265F ","\u265F ","\u265F ", "\u265F ", "\u265F ", "\u265F ", "\u265F ", "\u265F " , " 7" },
+            { "  ","  ","  ", "  ", "  ", "  ", "  ", "  " , " 6"  },
+            { "  ","  ","  ", "  ", "  ", "  ", "  ", "  " , " 5"  },
+            { "  ","  ","  ", "  ", "  ", "  ", "  ", "  " , " 4"  },
+            { "  ","  ","  ", "  ", "  ", "  ", "  ", "  " , " 3"  },
+            { "\u2659 ", "\u2659 ","\u2659 ", "\u2659 ", "\u2659 ", "\u2659 ", "\u2659 ", "\u2659 " , " 2" },
+            { "\u2656 ", "\u2658 ","\u2657 ", "\u2655 ", "\u2654 ", "\u2657 ", "\u2658 ", "\u2656 " , " 1" },
+        };
+       
+          
+                
+            
+        
+    }
+}
